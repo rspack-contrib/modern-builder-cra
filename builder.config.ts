13291b7341d7f3bac42816cf541334648678f8de
hardfist <yangjianzju@gmail.com>
2023-10-10 12:51:41 +0800

    chore: setup success

---
+++ builder.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@modern-js/builder-cli';
+
+// https://modernjs.dev/builder/en/api/index.html
+export default defineConfig({
+  html: {
+    template: './public/index.html',
+  },
+});
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,8 @@
-/// <reference types="react-scripts" />
+declare module '*.svg' {
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+
+  const content: string;
+  export default content;
+}
